f6f58a703bc87325f9f8a65643b725d02d32da09
Chris Mamon <chrisam1993@gmail.com>
2024-08-19 01:21:33 +1000

    feature: adding c implementation of fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,19 @@
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", 'nvim-telescope/telescope-fzf-native.nvim' },
     config = function()
-        require("telescope").setup({})
+        require("telescope").setup({
+            extensions = {
+                fzf = {
+                    fuzzy = true,                   -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true,    -- override the file sorter
+                    case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                }
+            }
+        })
         local extensions = require('telescope').extensions.menufacture
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>ff", extensions.find_files, { desc = "Telescope: find files" })
@@ -31,5 +41,6 @@ return {
                 desc =
                 "Telescope: git branch Lists all branches with log preview, checkout action <cr>, track action <C-t>, rebase action<C-r>, create action <C-a>, switch action <C-s>, delete action <C-d> and merge action <C-y>"
             })
+        require('telescope').load_extension('fzf')
     end
 }
