3e4fec5947040427b586f5caa066a93bcd1819bb
Chris Mamon <chrisam1993@gmail.com>
2024-08-18 20:40:44 +1000

    feature: added virtual edit toggle to keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -3,3 +3,18 @@ vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "<c-s>", ":w<cr>", { desc = "Save file" })
 vim.keymap.set("n", "<c-z>", "u", { desc = "Undo" })
+
+-- setup a toggle for the virtual edit command
+TOGGLE_VIRTUALEDIT = false
+vim.opt.virtualedit = nil
+vim.keymap.set("n", "<leader>ve", function()
+    if TOGGLE_VIRTUALEDIT then
+        print("INFO: setting virtualedit=nil")
+        vim.opt.virtualedit = nil
+        TOGGLE_VIRTUALEDIT = true
+    else
+        print("INFO: setting virtualedit=all")
+        vim.opt.virtualedit = "all"
+        TOGGLE_VIRTUALEDIT = true
+    end
+end, { desc = "Toggle virtualedit mode from nil <-> all" })
