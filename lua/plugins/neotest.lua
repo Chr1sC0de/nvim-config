2db0ce70df47d0293ded39b232b958d794bb25e9
Chris Mamon <chrisam1993@gmail.com>
2024-08-19 22:03:23 +1000

    feature: added neotest, neotest-python

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,30 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/neotest-python",
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter"
+    },
+    config = function()
+        require("neotest").setup({
+            adapters = {
+                require("neotest-python")({
+                    dap = { justMyCode = false },
+                }),
+            },
+        })
+        vim.keymap.set("n", "<leader>tn", function() require("neotest").run.run() end, { desc = "neotest: run nearest" })
+        vim.keymap.set("n", "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end,
+            { desc = "neotest: test file" })
+        vim.keymap.set("n", "<leader>td", function() require("neotest").run.run({ strategy = "dap" }) end,
+            { desc = "neotest: debug nearest test" })
+        vim.keymap.set("n", "<leader>ts", function() require("neotest").run.stop() end,
+            { desc = "neotest: stop the nearest test" })
+        vim.keymap.set("n", "<leader>ta", function() require("neotest").run.attach() end,
+            { desc = "neotest: attach the nearest test" })
+        vim.keymap.set("n", "<leader>tp", function() require("neotest").output.open() end,
+            { desc = "neotest: open test panel" })
+    end
+}
