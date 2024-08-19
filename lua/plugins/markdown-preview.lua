272250d3e3273ae4fbfecda823ed444c4804c001
Chris Mamon <chrisam1993@gmail.com>
2024-08-19 20:34:54 +1000

    feature: added markdown preview

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,17 @@
+return -- install with yarn or npm
+{
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+
+    config = function()
+        vim.keymap.set("n", "<leader>mp", ":MarkdownPreview<cr>", { desc = "Markdown Preview: Start" })
+        vim.keymap.set("n", "<leader>mx", ":MarkdownPreviewStop<cr>", { desc = "Markdown Preview: Stop" })
+        vim.keymap.set("n", "<leader>mt", ":MarkdownPreviewToggle<cr>", { desc = "Markdown Preview: Toggle" })
+    end
+
+}
