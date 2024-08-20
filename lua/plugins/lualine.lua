4793aa5061ea57c05f5a294ed9a5dd39fc2a00e8
Chris Mamon <chrisam1993@gmail.com>
2024-08-20 21:38:47 +1000

    feature: added fterm and updated lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,16 @@
 return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
+    init = function()
+        vim.g.lualine_laststatus = vim.o.laststatus
+        if vim.fn.argc(-1) > 0 then
+            -- set an empty statusline till lualine loads
+            vim.o.statusline = " "
+        else
+            -- hide the statusline on the starter page
+            vim.o.laststatus = 0
+        end
+    end,
     config = function()
         require("lualine").setup(
             {
@@ -10,13 +20,10 @@ return {
                     theme = 'catppuccin',
                     component_separators = { left = '', right = '' },
                     section_separators = { left = '', right = '' },
-                    disabled_filetypes = {
-                        statusline = {},
-                        winbar = {},
-                    },
+                    globalstatus = vim.o.laststatus == 3,
+                    disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter" } },
                     ignore_focus = {},
                     always_divide_middle = true,
-                    globalstatus = false,
                     refresh = {
                         statusline = 1000,
                         tabline = 1000,
@@ -42,7 +49,7 @@ return {
                 tabline = {},
                 winbar = {},
                 inactive_winbar = {},
-                extensions = { "neotree" }
+                extensions = { "neo-tree", "lazy" }
             }
         )
     end
