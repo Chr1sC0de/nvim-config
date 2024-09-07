addad2d8917d18952114e045f5adb5854caf232d
Chris Mamon <chrisam1993@gmail.com>
2024-09-08 09:22:19 +1000

    feature: null ls to none ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,15 +1,31 @@
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 return {
-    "jose-elias-alvarez/null-ls.nvim",
-    config = function()
-        require("null-ls").setup()
-    end,
+    "nvimtools/none-ls.nvim",
     requires = { "nvim-lua/plenary.nvim" },
     config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
+        local none_ls = require("null-ls")
+        none_ls.setup({
             sources = {
-                null_ls.builtins.formatting.markdownlint,
+                none_ls.builtins.formatting.markdownlint,
+                none_ls.builtins.formatting.prettierd,
             },
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds(
+                        {
+                            group = augroup,
+                            buffer = bufnr
+                        }
+                    )
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = bufnr })
+                        end
+                    })
+                end
+            end
         })
     end
 }
