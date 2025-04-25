1fc4b4cafe2dc15573d3aa9cb5d7446451060c32
Chris Mamon <chrisam1993@gmail.com>
2025-04-25 16:05:03 +1000

    update

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -11,12 +11,32 @@ return {
         require("neotest").setup({
             adapters = {
                 require("neotest-python")({
-                    dap = { justMyCode = true },
+                    -- Extra arguments for nvim-dap configuration
+                    -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+                    dap = { justMyCode = false },
+                    -- Command line arguments for runner
+                    -- Can also be a function to return dynamic values
+                    args = { "--log-level", "DEBUG" },
+                    -- Runner to use. Will use pytest if available by default.
+                    -- Can be a function to return dynamic value.
                     runner = "pytest",
-                    python = ".venv/bin/python"
-                }),
-            },
+                    -- Custom python path for the runner.
+                    -- Can be a string or a list of strings.
+                    -- Can also be a function to return dynamic value.
+                    -- If not provided, the path will be inferred by checking for
+                    -- virtual envs in the local directory and for Pipenev/Poetry configs
+                    python = ".venv/bin/python",
+                    -- Returns if a given file path is a test file.
+                    -- NB: This function is called a lot so don't perform any heavy tasks within it.
+                    -- is_test_file = function(file_path)
+                    -- end,
+                    -- !!EXPERIMENTAL!! Enable shelling out to `pytest` to discover test
+                    -- instances for files containing a parametrize mark (default: false)
+                    pytest_discover_instances = true,
+                })
+            }
         })
+
         vim.keymap.set("n", "<leader>tn", function() require("neotest").run.run() end, { desc = "neotest: run nearest" })
         vim.keymap.set("n", "<leader>tf", function() require("neotest").run.run(vim.fn.expand("%")) end,
             { desc = "neotest: test file" })
