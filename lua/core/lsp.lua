4428c0913139ae9c67631ab79f202fc002130e9d
Chris Mamon <chrisam1993@gmail.com>
2025-10-10 21:44:44 +1100

    update

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,118 +1,110 @@
 vim.lsp.enable({
-    -- "ty",
-    -- "pyrefly",
-    "basedpyright",
-    "bashls",
-    "docker_compose_language_service",
-    "dockerls",
-    "emmet_language_server",
-    "eslint",
-    "html",
-    "jinja_lsp",
-    "json_lsp",
-    "lua_ls",
-    "markdown_oxide",
-    "ruff",
-    "tailwindcss",
-    "ts_ls",
-    "htmx"
+	-- "ty",
+	-- "pyrefly",
+	"codebook",
+	"basedpyright",
+	"bashls",
+	"docker_compose_language_service",
+	"dockerls",
+	"emmet_language_server",
+	"eslint",
+	"html",
+	"jinja_lsp",
+	"json_lsp",
+	"lua_ls",
+	"markdown_oxide",
+	"ruff",
+	"tailwindcss",
+	"ts_ls",
+	"htmx",
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 -- enable dynamic registration for watched files
 capabilities.workspace.didChangeWatchedFiles = {
-    dynamicRegistration = true,
+	dynamicRegistration = true,
 }
 
-vim.lsp.config('lua_ls', {
-    settings = {
-        ['lua_ls'] = {
-            root_markers = {
-                ".git",
-                ".luacheckrc",
-                ".luarc.json",
-                ".luarc.jsonc",
-                ".stylua.toml",
-                "selene.toml",
-                "selene.yml",
-                "stylua.toml",
-            },
-        },
-    },
+vim.lsp.config("lua_ls", {
+	settings = {
+		["lua_ls"] = {
+			root_markers = {
+				".git",
+				".luacheckrc",
+				".luarc.json",
+				".luarc.jsonc",
+				".stylua.toml",
+				"selene.toml",
+				"selene.yml",
+				"stylua.toml",
+			},
+		},
+	},
 })
-vim.lsp.config(
-    'pyrefly', {
-        filetypes = { "python" },
-        settings = {
-            python = {
-                analysis = {
-                    typeCheckingMode = "basic", -- or "strict"
-                    autoImportCompletions = true
-                }
-            }
-        }
-    }
-)
 
-vim.lsp.config(
-    'basedpyright', {
-        capabilities = capabilities,
-        filetypes = { "python" },
-        settings = {
-            basedpyright = {
-                disableOrganizeImports = true,
-                analysis = {
-                    stubPath = os.getenv("HOME") .. "/Stubs"
-                }
-            },
-            python = {
-                analysis = {
-                    ignore = { '*' },
-                },
-            },
-        }
-    }
-)
+vim.lsp.config("pyrefly", {
+	filetypes = { "python" },
+	settings = {
+		python = {
+			analysis = {
+				typeCheckingMode = "basic", -- or "strict"
+				autoImportCompletions = true,
+			},
+		},
+	},
+})
 
-vim.lsp.config(
-    'jinja_lsp', {
-        filetypes = { 'jinja', 'htmldjango' },
-    }
-)
+vim.lsp.config("basedpyright", {
+	capabilities = capabilities,
+	filetypes = { "python" },
+	settings = {
+		basedpyright = {
+			disableOrganizeImports = true,
+			analysis = {
+				stubPath = os.getenv("HOME") .. "/Stubs",
+			},
+		},
+		python = {
+			analysis = {
+				ignore = { "*" },
+			},
+		},
+	},
+})
 
-vim.lsp.config(
-    'html', {
-        filetypes = { 'jinja', 'html', 'htmldjango' },
-    }
-)
+vim.lsp.config("jinja_lsp", {
+	filetypes = { "jinja", "htmldjango" },
+})
 
-vim.lsp.config(
-    'tailwindcss', {
-        filetypes = { 'jinja', 'html', 'htmldjango' },
-    }
-)
+vim.lsp.config("html", {
+	filetypes = { "jinja", "html", "htmldjango" },
+})
+
+vim.lsp.config("tailwindcss", {
+	filetypes = { "jinja", "html", "htmldjango" },
+})
 
 vim.diagnostic.config({
-    -- virtual_lines = true,
-    -- virtual_text = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
-    float = {
-        border = "rounded",
-        source = true,
-    },
-    signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = "󰅚 ",
-            [vim.diagnostic.severity.WARN] = "󰀪 ",
-            [vim.diagnostic.severity.INFO] = "󰋽 ",
-            [vim.diagnostic.severity.HINT] = "󰌶 ",
-        },
-        numhl = {
-            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-            [vim.diagnostic.severity.WARN] = "WarningMsg",
-        },
-    },
+	-- virtual_lines = true,
+	-- virtual_text = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+	float = {
+		border = "rounded",
+		source = true,
+	},
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
+			[vim.diagnostic.severity.WARN] = "WarningMsg",
+		},
+	},
 })
