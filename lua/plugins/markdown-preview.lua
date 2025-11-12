a442e990d8637f17824dce58ed9fa111a7189c38
Chris Mamon <chrisam1993@gmail.com>
2025-11-12 21:49:02 +1100

    update

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,17 +1,16 @@
 return -- install with yarn or npm
 {
-    "Carus11/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && yarn install",
-    init = function()
-        vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" },
+	"Carus11/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && yarn install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+	end,
+	ft = { "markdown" },
 
-    config = function()
-        vim.keymap.set("n", "<leader>mp", ":MarkdownPreview<cr>", { desc = "Markdown Preview: Start" })
-        vim.keymap.set("n", "<leader>mx", ":MarkdownPreviewStop<cr>", { desc = "Markdown Preview: Stop" })
-        vim.keymap.set("n", "<leader>mt", ":MarkdownPreviewToggle<cr>", { desc = "Markdown Preview: Toggle" })
-    end
-
+	config = function()
+		vim.keymap.set("n", "<leader>mdp", ":MarkdownPreview<cr>", { desc = "Markdown Preview: Start" })
+		vim.keymap.set("n", "<leader>mdx", ":MarkdownPreviewStop<cr>", { desc = "Markdown Preview: Stop" })
+		vim.keymap.set("n", "<leader>mdt", ":MarkdownPreviewToggle<cr>", { desc = "Markdown Preview: Toggle" })
+	end,
 }
