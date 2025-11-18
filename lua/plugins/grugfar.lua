f877e9f2e1f881acd8a7d80bf520f3975999f324
Chris Mamon <chrisam1993@gmail.com>
2025-11-18 22:09:02 +1100

    update

--- lua/plugins/grugfar.lua
+++ lua/plugins/grugfar.lua
@@ -1,20 +1,16 @@
 return {
     'MagicDuck/grug-far.nvim',
     opts = { headerMaxWidth = 80 },
+    lazy = false,
     cmd = "GrugFar",
     config = function()
         -- optional setup call to override plugin options
         -- alternatively you can set options with vim.g.grug_far = { ... }
-        require('grug-far').setup({
-            -- options, see Configuration section below
-            -- there are no required options atm
-            -- engine = 'ripgrep' is default, but 'astgrep' or 'astgrep-rules' can
-            -- be specified
-        });
+        require('grug-far').setup({});
         vim.keymap.set({ "n", "v" }, "<leader>sr", function()
             local grug = require("grug-far")
             local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
-            grug.grug_far({
+            grug.with_visual_selection({
                 transient = true,
                 prefills = {
                     filesFilter = ext and ext ~= "" and "*." .. ext or nil,
