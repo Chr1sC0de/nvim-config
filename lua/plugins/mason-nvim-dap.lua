ed2af224ec61e7b1e127c48d1dec36c6b49de173
Chris Mamon <chrisam1993@gmail.com>
2025-12-07 16:34:18 +1100

    update

--- lua/plugins/mason-nvim-dap.lua
+++ lua/plugins/mason-nvim-dap.lua
@@ -1,6 +1,7 @@
 return {
 	"jay-babu/mason-nvim-dap.nvim",
 	dependencies = "mason.nvim",
+	lazy = false,
 	cmd = { "DapInstall", "DapUninstall" },
 	ensure_installed = { "debugpy", "bash-debug-adapter" },
 	automatic_installation = true,
