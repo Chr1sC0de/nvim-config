7371bf0f1e2348915e1556009e67527993fe0b2f
Chris Mamon <chrisam1993@gmail.com>
2026-01-13 10:25:03 +0000

    update

--- lua/plugins/iron-nvim.lua
+++ lua/plugins/iron-nvim.lua
@@ -35,7 +35,7 @@ return {
 				dap_integration = true,
 				-- How the repl window will be displayed
 				-- See below for more information
-				repl_open_cmd = view.right("%40"),
+				repl_open_cmd = view.split.vertical.botright("50%"),
 
 				-- repl_open_cmd can also be an array-style table so that multiple
 				-- repl_open_commands can be given.
