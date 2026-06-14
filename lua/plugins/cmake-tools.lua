02379a215dcb3ff1ea81a191d98d9c852bfd8092
Chris Mamon <chrisam1993@gmail.com>
2026-06-14 23:48:33 +1000

    update

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -1,1 +1,28 @@
-return { "Civitasv/cmake-tools.nvim", opts = {} }
+return {
+	"Civitasv/cmake-tools.nvim",
+	opts = {},
+	config = function()
+		require("cmake-tools").setup({
+			cmake_executor = { -- executor to use
+				name = "overseer", -- name of the executor
+				default_opts = { -- a list of default and possible values for executors
+					overseer = {
+						on_new_task = function(task)
+							require("overseer").open({ enter = false, direction = "bottom" })
+						end, -- a function that gets overseer.Task when it is created, before calling `task:start`
+					},
+				},
+			},
+			cmake_runner = { -- runner to use
+				name = "overseer", -- name of the runner
+				default_opts = { -- a list of default and possible values for runners
+					overseer = {
+						on_new_task = function(task)
+							require("overseer").open({ enter = false, direction = "bottom" })
+						end, -- a function that gets overseer.Task when it is created, before calling `task:start`
+					},
+				},
+			},
+		})
+	end,
+}
